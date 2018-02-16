f467514b3612fa4efaaa6a9b7e406eba9e742777
John-Gee <john.ettedgui@gmail.com>
2018-02-16 04:03:49 -0800

    Split the torrents and sets mode

--- MAME.py
+++ MAME.py
@@ -10,30 +10,46 @@ import sethandling
 import transmission
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Mame script')
-    parser.add_argument('liste', metavar='list',
-                    help='The Mame list generated with -listxml')
-    parser.add_argument('folder', metavar='folder',
-                    help='The Mame ROMs folder on disk')
-    parser.add_argument('RID', metavar='RID',
-                    help='The ROMs Torrent ID in transmission (starts at 1)')
-    parser.add_argument('CID', metavar='CID',
-                    help='The CHDs Torrent ID in transmission (starts at 1)')
-    args  = parser.parse_args()
-    liste  = args.liste
+def sets(args, parents):
     folder = args.folder
-    RID    = args.RID
-    CID    = args.CID
-
-    blacklist = ('mach3', 'cobra', 'usvsthem', 'firefox', 'cubeqst', 'gtfore05', 'bmfinal', 'gtfore04', 'vaportrx', 'gtfore03', 'turrett', 'calspeed', 'dstage', 'ddr2m', 'gtfore06', 'bm', 'ddr3mk', 'dsf', 'gtf', 'popn')
-    romset = mameparser.get_romset(liste, blacklist)
-    parents = tuple(sorted(romset.keys()))
-    print('Number of parents to get: {0}'.format(len(parents)))
     sethandling.keep_only_set(parents, folder, False)
     print('Set Handling done')
+
+
+def torrents(args, parents):
+    CID = args.CID
+    RID = args.RID
     transmission.set_them_folders(CID, parents)
     print('set them folders done')
     parents = tuple(map(lambda x: x + '.zip', sorted(romset.keys())))
     transmission.set_them_files(RID, parents)
     print('set them files done')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Mame script')
+    parser.add_argument('liste', metavar='list',
+                    help='The Mame list generated with -listxml')
+    subparsers = parser.add_subparsers(help='Pick the desired mode')
+
+    torrents_parser = subparsers.add_parser('torrent',
+                        help='Scripts for Transmission')
+    sets_parser    = subparsers.add_parser('sets',
+                        help='Scripts for sets handling')
+
+    sets_parser.add_argument('folder', metavar='folder',
+                    help='The Mame ROMs folder on disk')
+    sets_parser.set_defaults(func=sets)
+    torrents_parser.add_argument('RID', metavar='RID',
+                    help='The ROMs Torrent ID in transmission (starts at 1)')
+    torrents_parser.add_argument('CID', metavar='CID',
+                    help='The CHDs Torrent ID in transmission (starts at 1)')
+    torrents_parser.set_defaults(func=torrents)
+    args   = parser.parse_args()
+
+    liste  = args.liste
+
+    blacklist = ('mach3', 'cobra', 'usvsthem', 'firefox', 'cubeqst', 'gtfore05', 'bmfinal', 'gtfore04', 'vaportrx', 'gtfore03', 'turrett', 'calspeed', 'dstage', 'ddr2m', 'gtfore06', 'bm', 'ddr3mk', 'dsf', 'gtf', 'popn')
+    romset = mameparser.get_romset(liste, blacklist)
+    parents = tuple(sorted(romset.keys()))
+    print('Number of parents to get: {0}'.format(len(parents)))
+    args.func(args, parents)
--- sethandling.py
+++ sethandling.py
@@ -12,20 +12,20 @@ def splitzip_to_merged7z(romset):
         os.chdir(name)
         empty = True
         for child in romset[name]:
-            path = "../{0}.zip".format(child.get("name"))
+            path = '../{0}.zip'.format(child.get('name'))
             if(os.path.exists(path)):
-                subprocess.run("unzip -n {0}".format(path),
+                subprocess.run('unzip -n {0}'.format(path),
                                shell=True, check=True)
                 empty = False
 
-        os.chdir("..")
+        os.chdir('..')
         if (empty):
-            subprocess.run("touch MAME/empty/{0}.7z ".format(name),
+            subprocess.run('touch MAME/empty/{0}.7z '.format(name),
                            shell=True, check=True)
         else:
-            subprocess.run("7z a -t7z -mx9 {0}.7z {0}".format(name),
+            subprocess.run('7z a -t7z -mx9 {0}.7z {0}'.format(name),
                            shell=True, check=True)
-            subprocess.run("mv {0}.7z MAME/".format(name),
+            subprocess.run('mv {0}.7z MAME/'.format(name),
                            shell=True, check=True)
         shutil.rmtree(name)
 
@@ -33,20 +33,21 @@ def splitzip_to_merged7z(romset):
 def keep_only_set(romset, folder, remove):
     i = 0
     j = 0
+    print('the remove flag is: {0}'.format(remove))
     for filename in sorted(os.listdir(folder)):
         noext=os.path.splitext(filename.lower())[0]
         if (noext not in romset):
             i += 1
-            path = "{0}{1}".format(folder, filename)
+            path = '{0}{1}'.format(folder, filename)
             if (os.path.isdir(path)):
                 if (remove):
                     shutil.rmtree(path)
-                print("remove dir: {0}".format(path))
+                print('remove dir: {0}'.format(path))
             else:
                 if (remove):
                     os.remove(path)
-                print("remove file: {0}".format(path))
+                print('remove file: {0}'.format(path))
         else:
             j += 1
-    print("Removed {0} files".format(i))
-    print("Found {0} files from the romset".format(j))
+    print('Removed {0} files'.format(i))
+    print('Found {0} files from the romset'.format(j))
